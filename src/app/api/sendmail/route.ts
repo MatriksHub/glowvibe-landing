24761614828338982f6a5ca005bcaf06f8d43ebe
Culmerin Inc <aniekan.g.edet@gmail.com>
2025-02-10 09:39:06 +0100

    fix: email client

--- src/app/api/sendmail/route.ts
+++ src/app/api/sendmail/route.ts
@@ -1,33 +1,33 @@
-// import { sendEmail } from "@/lib/mailtrap.utils"
+import { sendEmail } from "@/lib/mailtrap.utils"
 
 
-// export async function POST() {
-//     const sender = {
-//         name: 'Glowvibes',
-//         address: 'no-reply@example.com',
-//     }
+export async function POST() {
+    const sender = {
+        name: 'Glowvibes',
+        address: 'no-reply@example.com',
+    }
 
-//     const recipients = [{
-//         name: 'John Doe',
-//         address: 'john.doe@example.com',
-//     }]
+    const recipients = [{
+        name: 'John Doe',
+        address: 'john.doe@example.com',
+    }]
 
-//     try {
-//         const result = await sendEmail({
-//             sender,
-//             recipients, 
-//             subject: 'Welcome to Glowvibes',
-//             message: 'You are welcome to our platform'
-//         })
+    try {
+        const result = await sendEmail({
+            sender,
+            recipients, 
+            subject: 'Welcome to Glowvibes',
+            message: 'You are welcome to our platform'
+        })
 
-//         return Response.json({
-//             accepted: result.accepted,
-//         })
-//     } catch (error:any) {
-//         console.error(error)
-//         return Response.json({
-//             message: 'Unable to send email this time',
+        return Response.json({
+            accepted: result.accepted,
+        })
+    } catch (error) {
+        console.error(error)
+        // return Response.json({
+        //     message: 'Unable to send email this time',
             
-//         }, {status: 500})
-//     }
-// }
+        // }, {status: 500})
+    }
+}
