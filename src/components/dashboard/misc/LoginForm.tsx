a03354a7f6e1d463049681629281cdc2b99be414
Culmerin Inc <aniekan.g.edet@gmail.com>
2025-02-19 19:57:41 +0100

    fix: remove toast

--- src/components/dashboard/misc/LoginForm.tsx
+++ src/components/dashboard/misc/LoginForm.tsx
@@ -7,7 +7,7 @@ import { Label } from "@/components/ui/label"
 import { useState } from "react"
 import { useRouter } from "next/navigation"
 import { supabase } from "@/lib/supabase"
-import { toast } from "sonner"
+import { toast } from "react-toastify"
 
 export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<"div">) {
 
--- src/components/dashboard/misc/VerifyForm.tsx
+++ src/components/dashboard/misc/VerifyForm.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from "react"
 import { useRouter } from "next/navigation"
-import { toast } from "sonner"
+import { toast } from "react-toastify"
 
 export function VerifyForm({ className, ...props }: React.ComponentPropsWithoutRef<"div">) {
 
