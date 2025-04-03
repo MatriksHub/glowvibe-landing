58e034fb4e6dd6a9a7aa2d379bd714e29c2d4d65
Aniekan George Edet <aniekan.g.edet@gmail.com>
2025-04-03 19:35:35 +0100

    fix: banner padding for mobile screens

--- src/components/Downloadapp.tsx
+++ src/components/Downloadapp.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 function Downloadapp() {
   return (
     <section className='pb-[50px] relative' id='download'>
-      <div className='container md:max-w-[90%] lg:max-w-[85%] xl:max-w-[85%] 3xl:max-w-[80%]'>
-        <div className='bg-primary flex flex-col justify-center items-center rounded-2xl text-center p-[60px] mt-[-150px]'>
+      <div className='container max-w-[95%] md:max-w-[90%] lg:max-w-[85%] xl:max-w-[85%] 3xl:max-w-[80%]'>
+        <div className='bg-primary flex flex-col justify-center items-center rounded-2xl text-center py-[60px] px-[40px] mt-[-150px] md:px-[50px] lg:px-[60px]'>
           <h2 className='text-[35px] font-bold text-white lg:text-[45px]'>Ready to Take Charge of Your Sexual Health?</h2>
 
           <p className='text-[16px] mt-[14px] mb-[50px] text-white lg:text-[18px]'>Download GlowVibe App today and start exploring in a safe, supportive, and private space.</p>
